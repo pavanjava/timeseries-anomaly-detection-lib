179db76fa620ef8dab576079bb4b396fbd6c97e4
Pavan Mantha <manthapavankumar11@gmail.com>
2023-04-11 10:44:56 +0530

    committing the basic version of library

---
+++ PracticeExamples/DataScience_Practice/anomaly-detect-library/src/core/utils.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+class LoadDataSet:
+    def load(self, file: str, delimiter: str = ","):
+        if len(delimiter) != 0:
+            df = pd.read_csv(file, delimiter=delimiter)
+            return df
+        else:
+            df = pd.read_csv(file)
+            return df
